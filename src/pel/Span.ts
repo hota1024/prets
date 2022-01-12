088d00d85164639a8a077c13f13b82ef5771e81e
= <hotalog@hota1024.com>
2022-01-12 19:48:00 +0900

    🏷️add Span class

---
+++ src/pel/Span.ts
@@ -0,0 +1,24 @@
+/**
+ * Span class.
+ */
+export class Span {
+  /**
+   * Span constructor.
+   *
+   * @param start start number.
+   * @param end end number.
+   */
+  constructor(public readonly start: number, public readonly end: number) {}
+
+  /**
+   * merge two spans and returns merged new Span instance.
+   *
+   * @param span span.
+   */
+  merge(span: Span): Span {
+    return new Span(
+      Math.min(this.start, span.start),
+      Math.max(this.end, span.end)
+    )
+  }
+}
